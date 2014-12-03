fdd187541671c5b83ef49b84b48c71e54f3bb3b6
clearosindbsingh <bharat.xtreem@gmail.com>
2014-12-03 02:43:34 -0700

    Remove Blank <li> from header menu

--- core/page.php
+++ core/page.php
@@ -659,7 +659,7 @@ function _get_header($page, $menus = array())
                               <li><a role='menuitem' href='/app/base/session/logout'>Sign out</a></li>
                             </ul>
                           </li>
-                            " : "<li></li><li></li><li></li>") . "
+                            " : "") . "
                     </ul>
                     <div class='ClearOS logo1 " . (($page['current_basename'] == '') ? "active" : "") . "'>
                         <a href='#' id='clearos-home' class='ci-ClearOS'>&nbsp;</a>
