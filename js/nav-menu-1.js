8f4eade4ba4fb94289ab589ee56f72608eef4ae3
clearosindbsingh <bharat.xtreem@gmail.com>
2015-07-29 10:10:33 -0600

    Updates on the left nav issues. updated left menus hiding submenu, and opening menus with hover.

--- js/nav-menu-1.js
+++ js/nav-menu-1.js
@@ -1,5 +1,5 @@
 /**
- * Marketplace javascript helper.
+ * Menu type 1 helper.
  *
  * @category  Theme
  * @package   ClearOS
@@ -9,4 +9,71 @@
  * @link      http://www.clearfoundation.com/docs/developer/theming/
  */
 
-/* Main menu uses boostrap file. */
+$(document).ready(function() {
+    if (jQuery(".theme-wizard-active").length == 0) {
+        if (jQuery(window).width() < 801) {
+            var left_nav = jQuery(".left_nav").html();
+            jQuery(".left_nav").remove();
+            jQuery(".small_menu").append("<ul class='left_nav'>"+left_nav+"</ul>");
+        } else {
+            var left_nav = jQuery(".left_nav").html();
+            jQuery(".left_nav").remove();
+            jQuery(".sidebar-form").after("<ul class='left_nav'>"+left_nav+"</ul>");
+        }
+    }
+        function left_menu_js(){
+            if (jQuery(window).width() < 801) {
+                var left_nav = jQuery(".left_nav").html();
+                jQuery(".left_nav").remove();
+                jQuery(".small_menu").append("<ul class='left_nav'>"+left_nav+"</ul>");
+                jQuery('.left_nav li a').click(function() {
+                    if(!jQuery(this).parent().hasClass("active")) {
+                        jQuery(this).parent().siblings('li').removeClass('active').find('>ul').slideUp('slow').find('li').removeClass('active');
+                        jQuery(this).parent().addClass('active').find('>ul').slideDown('slow');
+                    } else {
+                        jQuery(this).parent().removeClass('active').find('>ul').slideUp('slow').find('li').removeClass('active');
+                    }
+                });
+            } else {
+                var left_nav = jQuery(".left_nav").html();
+                jQuery(".left_nav").remove();
+                jQuery(".sidebar-form").after("<ul class='left_nav'>"+left_nav+"</ul>");
+                jQuery('.left_nav li a').click(function(){
+                    if(!jQuery(this).parent().hasClass("active")) {
+                        jQuery(this).parent().siblings('li').removeClass('active').find('>ul').slideUp('slow').find('li').removeClass('active');
+                        jQuery(this).parent().addClass('active').find('>ul').slideDown('slow');
+                    } else {
+                        jQuery(this).parent().removeClass('active').find('>ul').slideUp('slow').find('li').removeClass('active');
+                    }
+                });
+            }
+
+    }
+    
+    jQuery(window).resize(function(){
+        //left_menu_js();
+    });
+    //left_menu_js();
+
+    /*
+        Left sidebar Start
+    */
+
+    // open first menu by-default 
+    // if(!jQuery('.left_nav li').hasClass("active")){
+    //     jQuery('.left_nav li:first a:first').trigger('click');
+    // } else {
+    //     jQuery('.left_nav li.active').find('>ul.sub_menu').slideDown('slow');
+    // }
+	//jQuery(".sub_menu li.active > ul").slideDown('slow');
+    var asidewidth = jQuery(".marketPlaceSection aside").width();
+    jQuery(".left_nav").css('width', asidewidth);
+    
+    /*
+        Left sidebar End
+    */
+    jQuery(".navbar-toggle").click(function(){
+        jQuery(".small_menu").slideToggle();
+    });
+});	
+
