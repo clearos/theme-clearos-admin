3949dabbe4ba86a25d66c3d5846eb35e9b0cbe49
Benjamin Chambers <bchambers@clearfoundation.com>
2015-11-10 14:53:55 -0500

    Improved graphical console [Tracker #2176]

--- deploy/info.php
+++ deploy/info.php
@@ -7,7 +7,7 @@ $package['name'] = 'theme-clearos-admin';
 $package['title'] = 'ClearOS Admin Theme';
 $package['description'] = 'The ClearOS Admin Theme';
 
-$package['version'] = '1.2';
+$package['version'] = '1.3';
 $package['release'] = '0.0';
 
 $package['vendor'] = 'ClearFoundation';
